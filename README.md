fb40aa3fb655a15973c529a9937e0945cdf5f08d
F16Gaming <adam.hellberg95@gmail.com>
2012-01-04 19:14:21 +0100

    Added logging and Steam unloading.

--- README.md
+++ README.md
@@ -15,4 +15,5 @@ Copyright © 2012 by Sijmen Schoon (Vijfhoek) and Adam Hellberg (F16Gaming).
 Dependencies
 ------------
 
+[Log4Net](http://logging.apache.org/log4net/) is required for this to work, you can download the source and binaries from [their website](http://logging.apache.org/log4net/).
 [OpenSteamWorks (OSW)](http://opensteamworks.org/) is required for this to work, you can download the source from [their mercurial repo](http://hg.opensteamworks.org/open-steamworks/src).
---
+++ Sharpcraft.Logging/LoggerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+using log4net;
+using log4net.Config;
+
+namespace Sharpcraft.Logging
+{
+	public static class LoggerManager
+	{
+		private static bool _loaded;
+		private const string ConfigFile = @"configs\LoggerConfig.xml";
+
+		public static void LoadConfig()
+		{
+			XmlConfigurator.Configure(new FileInfo(ConfigFile));
+			_loaded = true;
+		}
+
+		public static ILog GetLogger(object sender)
+		{
+			if (!_loaded)
+				LoadConfig();
+			return LogManager.GetLogger(sender.GetType().ToString() == "System.RuntimeType" ? (Type) sender : sender.GetType());
+		}
+	}
+}
