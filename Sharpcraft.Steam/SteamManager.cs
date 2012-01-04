78820bf21f455ad4128146a5b252e77a9def3969
F16Gaming <adam.hellberg95@gmail.com>
2012-01-04 04:10:53 +0100

    Update SteamManager.

--- Sharpcraft.Steam/SteamManager.cs
+++ Sharpcraft.Steam/SteamManager.cs
@@ -9,10 +9,27 @@ namespace Sharpcraft.Steam
 {
 	public static class SteamManager
 	{
-		internal static ISteamClient010 SteamClient;
+		internal static ISteamClient010 Client;
+		internal static ISteamFriends002 Friends;
 		internal static int Pipe;
 		internal static int User;
 
-		
+		public static bool Init()
+		{
+			if (!Steamworks.Load())
+				throw new SteamException("Steamworks failed to load.", SteamExceptionType.SteamworksLoadFail);
+
+			Client = Steamworks.CreateInterface<ISteamClient010>("SteamClient010");
+			if (Client == null)
+				throw new SteamException("Steamclient failed to load! Is the client running? (Sharpcraft.Steam.SteamManager.Client == null!)",
+				                         SteamExceptionType.SteamLoadFail);
+
+			Pipe = Client.CreateSteamPipe();
+			User = Client.ConnectToGlobalUser(Pipe);
+
+			Friends = Steamworks.CastInterface<ISteamFriends002>(Client.GetISteamFriends(User, Pipe, "SteamFriends002"));
+
+			return true;
+		}
 	}
 }
