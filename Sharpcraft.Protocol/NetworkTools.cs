9cba1316c40a208c9091cec787aa09e53bb87bfd
Vijfhoek <vijfhoek@megapowers.net>
2012-01-04 17:06:54 +0100

    Forgot to add NetworkTools.cs

---
+++ Sharpcraft.Protocol/NetworkTools.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sharpcraft.Protocol
+{
+	class NetworkTools
+	{
+		private NetworkStream _stream;
+		public NetworkTools(NetworkStream stream)
+		{
+			_stream = stream;
+		}
+
+		public string ReadString()
+		{
+			byte[] bteStringLength = { (byte)_stream.ReadByte(), (byte)_stream.ReadByte() };
+			short stringLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(bteStringLength, 0));
+			string str = "";
+			for (short s = 0; s < stringLength; s++)
+			{
+				byte[] bte = { (byte)_stream.ReadByte(), (byte)_stream.ReadByte() };
+				str += Encoding.BigEndianUnicode.GetString(bte);
+			}
+
+			return str;
+		}
+
+		public void WriteString(string s)
+		{
+			_stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)s.Length)), 0, 2);
+			byte[] bteUsername = Encoding.BigEndianUnicode.GetBytes(s);
+			_stream.Write(bteUsername, 0, bteUsername.Length);
+		}
+
+
+		public Int16 ReadInt16()
+		{
+			var bte = new byte[2];
+			_stream.Read(bte, 0, bte.Length);
+			return IPAddress.NetworkToHostOrder(BitConverter.ToInt16(bte, 0));
+		}
+
+		public Int32 ReadInt32()
+		{
+			var bte = new byte[4];
+			_stream.Read(bte, 0, bte.Length);
+			return IPAddress.NetworkToHostOrder(BitConverter.ToInt16(bte, 0));
+		}
+
+		public Int64 ReadInt64()
+		{
+			var bte = new byte[8];
+			_stream.Read(bte, 0, bte.Length);
+			return IPAddress.NetworkToHostOrder(BitConverter.ToInt16(bte, 0));
+		}
+
+		public void WriteByte(byte i)
+		{
+			_stream.WriteByte(i);
+		}
+
+		public void WriteInt16(Int16 i)
+		{
+			byte[] bte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(i));
+			_stream.Write(bte, 0, bte.Length);
+		}
+
+		public void WriteInt32(Int32 i)
+		{
+			byte[] bte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(i));
+			_stream.Write(bte, 0, bte.Length);
+		}
+
+		public void WriteInt64(Int64 i)
+		{
+			byte[] bte = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(i));
+			_stream.Write(bte, 0, bte.Length);
+		}
+
+
+		public void StreamSkip(int amount)
+		{
+			for (int i = 0; i < amount; i++)
+				_stream.ReadByte();
+		}
+	}
+}
