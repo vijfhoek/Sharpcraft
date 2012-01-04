cd390c9c10244109124634d7094b11fd52c600e5
F16Gaming <adam.hellberg95@gmail.com>
2012-01-04 03:39:25 +0100

    Assembly update, added stub for Steam functionality.

---
+++ Sharpcraft.Steam/SteamManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Steam4NET;
+
+namespace Sharpcraft.Steam
+{
+	public static class SteamManager
+	{
+		internal static ISteamClient010 SteamClient;
+		internal static int Pipe;
+		internal static int User;
+
+		
+	}
+}
