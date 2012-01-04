45c9a6c6b6fa036b1afb109b23e5392b39fe6694
Vijfhoek <vijfhoek@megapowers.net>
2012-01-04 04:41:58 +0100

    Created a separate code file for Packets

--- Sharpcraft/Protocol.cs
+++ Sharpcraft/Protocol.cs
@@ -15,33 +15,6 @@ namespace Sharpcraft
 		BigEndian
 	}
 
-	// TODO Move Packets to a sep. .cs file
-	public class Packet
-	{
-		public byte packetID;
-	}
-
-	public class Packet0 : Packet
-	{
-		public Int32 keepAliveID;
-	}
-
-	public class Packet1 : Packet
-	{
-		public Int32 entityID;
-		public Int64 mapSeed;
-		public Int32 gamemode;
-		public sbyte dimension;
-		public sbyte difficulty;
-		public byte worldHeight;
-		public byte maxPlayers;
-	}
-
-	public class Packet2 : Packet
-	{
-		public string connectionHash;
-	}
-
 	public class Protocol
 	{
 		TcpClient client = new TcpClient();
