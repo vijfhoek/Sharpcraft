6fe0d2c63b6aeb64416f15527d1960921afab900
F16Gaming <adam.hellberg95@gmail.com>
2012-01-05 05:31:37 +0100

    Added catch for SocketException.

--- Sharpcraft/Program.cs
+++ Sharpcraft/Program.cs
@@ -4,11 +4,10 @@
  * All Rights Reserved.
  */
 
-//#define DEVELOPMENT
+#define DEVELOPMENT
 
 using System;
 using System.IO;
-
 using log4net;
 
 using Sharpcraft.Logging;
@@ -18,47 +17,55 @@ namespace Sharpcraft
 #if WINDOWS || XBOX
 	static class Program
 	{
-		private static ILog _log = LoggerManager.GetLogger(typeof(Program));
+		private static readonly ILog Log = LoggerManager.GetLogger(typeof(Program));
 
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		static void Main(string[] args)
 		{
-			_log.Info("!!! APPLICATION LOAD !!!");
-			_log.Info("Sharpcraft is loading...");
+			Log.Info("!!! APPLICATION LOAD !!!");
+			Log.Info("Sharpcraft is loading...");
 			try
 			{
-				_log.Debug("Creating protocol...");
+				Log.Debug("Creating protocol...");
 				var protocol = new Protocol.Protocol("localhost", 25565);
 
-				_log.Debug("Sending handshake packet.");
+				Log.Debug("Sending handshake packet.");
 				protocol.PacketHandshake("Sharpcraft");
 				protocol.GetPacket();
-				_log.Debug("Sending login request.");
+				Log.Debug("Sending login request.");
 				protocol.PacketLoginRequest(22, "Sharpcraft");
 				protocol.GetPacket();
 
 				using (var game = new Sharpcraft())
 				{
-					_log.Debug("Running game (Game.Run()).");
+					Log.Debug("Running game (Game.Run()).");
 					game.Run();
 				}
-				_log.Info("!!! APPLICATION EXIT !!!");
+				Log.Info("!!! APPLICATION EXIT !!!");
 			}
 			catch(FileNotFoundException ex)
 			{
-				_log.Fatal("Required file \"" + ex.FileName + "\" not found! Application is exiting...");
+				Log.Fatal("Required file \"" + ex.FileName + "\" not found! Application is exiting...");
+				Environment.Exit(1);
+			}
+			catch(System.Net.Sockets.SocketException ex)
+			{
+				Log.Error("Failed to connect to target server, " + ex.GetType() + " was thrown.");
+				Log.Error(ex.GetType() + ": " + ex.Message);
+				Log.Error("Stack Trace:\n" + ex.StackTrace);
+				Log.Error("Exiting...");
 				Environment.Exit(1);
 			}
 #if !DEVELOPMENT
 			catch(Exception ex)
 			{
-				_log.Fatal("Unknown exception " + ex.GetType() + " thrown. Details below:");
-				_log.Fatal("Exception " + ex.GetType());
-				_log.Fatal("Message: " + ex.Message);
-				_log.Fatal("Stack Trace:\n" + ex.StackTrace);
-				_log.Fatal("Cannot continue application execution, exiting...");
+				Log.Fatal("Unknown exception " + ex.GetType() + " thrown. Details below:");
+				Log.Fatal("Exception " + ex.GetType());
+				Log.Fatal("Message: " + ex.Message);
+				Log.Fatal("Stack Trace:\n" + ex.StackTrace);
+				Log.Fatal("Cannot continue application execution, exiting...");
 				Environment.Exit(1);
 			}
 #endif
