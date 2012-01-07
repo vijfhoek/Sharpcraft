ca7d716eca2f0db70f2d5bec1791daa4e67f23f7
F16Gaming <adam.hellberg95@gmail.com>
2012-01-07 13:09:37 +0100

    Updated launcher background.

--- Sharpcraft/Properties/AssemblyInfo.cs
+++ Sharpcraft/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.98.1236")]
-[assembly: AssemblyFileVersion("0.1.98.1236")]
+[assembly: AssemblyVersion("0.1.99.1306")]
+[assembly: AssemblyFileVersion("0.1.99.1306")]
