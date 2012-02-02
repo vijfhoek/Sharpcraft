fdbbd624a1e755c00e0a7694856d921a4e3f5206
Vijfhoek <vijfhoek35@gmail.com>
2012-02-02 23:04:41 +0100

    SlotData added

---
+++ Sharpcraft.Networking/SlotData.cs
@@ -0,0 +1,25 @@
+﻿using LibNbt.Tags;
+
+// Temporarily disabled the notification that there is no docs
+// ReSharper disable CSharpWarnings::CS1591
+
+namespace Sharpcraft.Networking
+{
+	public class SlotData
+	{
+		public short ItemID;
+		public byte ItemCount;
+		public short ItemDamage;
+		public NbtCompound ItemEnchantments;
+
+		public SlotData(short itemID = -1, byte itemCount = 0, short itemDamage = 0, NbtCompound itemEnchantments = null)
+		{
+			ItemID = itemID;
+			ItemCount = itemCount;
+			ItemDamage = itemDamage;
+			ItemEnchantments = itemEnchantments;
+		}
+	}
+}
+
+// ReSharper restore CSharpWarnings::CS1591
